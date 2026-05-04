974340ffae148597ec2fb82f1ff4d8abc9db3ebd
Daved <Cazamirloucas@gmail.com>
2026-05-04 11:35:44 +0300

    Refactor lexer.js to improve tokenization logic

--- SOURCE/V1/lexer.js
+++ SOURCE/V1/lexer.js
@@ -3,24 +3,32 @@ let source = `vrb("name", txt, "Alice")`; // the source. Currently an example
 let tokens = []; // the token list
 let pos = 0; // the 'cursor', moves by 1 repeatedly, to the next digit. Does not work alone, only as source[pos]
 let number = ' ' // for tokenizing numbers
+let string1 = ` ` // for tokenizing  strings with double quotes
+let string2 = ` ` // for tokenizing strings with single quotes
 //THE TOKENS
 while(pos < source.length) {
   const char = source[pos];
   pos++;
-  if(char === '(') {tokens.push({type: "LPAREN", value: '('})};
-  if(char === ')') {tokens.push({type: "RPAREN", value: ')'})};
-  if(char === '[') {tokens.push({type: "LBRACK", value: '['})};
-  if(char === ']') {tokens.push({type: "RBRACK", value: ']'})};
-  if(char === '{') {tokens.push({type: "LBRACE", value: '{'})};
-  if(char === '}') {tokens.push({type: "RBRACE", value: '}'})};
-  if(char === ',') {tokens.push({type: "COMMA", value: ','})};
-  if(char === ' ') {pos++; continue};
-  if(char >= '0' && char <= '9') {
-    while(source[pos] >= '0' && source[pos] <= 9) {number += source[pos]; pos++};
-    tokens.push({type: "NUMBER", value: number})
-  }
-  if(char === '"' || char === "'") {
-    // incomplete, but i gotta do something and i do NOT wanna lose progress 
-  }
+  if(char === '(') {tokens.push({type: "LPAREN", value: '('})};                            //---
+  if(char === ')') {tokens.push({type: "RPAREN", value: ')'})};                           //
+  if(char === '[') {tokens.push({type: "LBRACK", value: '['})};                          //
+  if(char === ']') {tokens.push({type: "RBRACK", value: ']'})};                         // punctuation
+  if(char === '{') {tokens.push({type: "LBRACE", value: '{'})};                        //
+  if(char === '}') {tokens.push({type: "RBRACE", value: '}'})};                       //
+  if(char === ',') {tokens.push({type: "COMMA", value: ','})};                       //---                          
+  if(char === ' ') {pos++; continue};                                               // whitespace
+  if(char >= '0' && char <= '9') {                                                 //---
+    while(source[pos] >= '0' && source[pos] <= 9) {number += source[pos]; pos++}; // numbers
+    tokens.push({type: "NUMBER", value: number})                                 //
+  }                                                                             //---
+  if(char === `"`) {                                                           //---
+    while(source[pos] !== `"`) {string1 += source[pos]; pos++};               //
+    tokens.push({type: "TEXT", value: string1});                             //
+  }                                                                         // text/strings
+  if(char === `'`) {                                                       //
+    while(source[pos] !== `'`) {string2 += source[pos]; pos++};           //
+    tokens.push({type: "TEXT", value: string2});                         //
+  }                                                                     //---
+}
     
   
