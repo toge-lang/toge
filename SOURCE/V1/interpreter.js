af9e533f25d6bdeebc586cb14aef31c0ff12c493
TOGE <cazamirloucas+toge@gmail.com>
2026-05-25 09:52:19 +0300

    Update interpreter.js

--- SOURCE/V1/interpreter.js
+++ SOURCE/V1/interpreter.js
@@ -1,106 +1,71 @@
 // evaluating AST ---
 let database = {};
 const functionSignatures = { //rules of arguments for each function, 'signatures' if you will
-  "wait": { 
-    args: [
-      {pos: 0, name: "unit", required: false},
-      {pos: 1, name: "duration", required: true, type: ["int", "dint"]}
-    ]
-  },
-  "wrt": {
-    args: [{pos: 0, required: true}],
-    minArgs: 1,
-    maxArgs: Infinity
-  },
-  "tlk": {
-    args: [{pos: 0, name: "type", required: true}],
-    minArgs: 1,
-    maxArgs: Infinity
-  },
-  "ret": {
-    minArgs: 0,
-    maxArgs: Infinigy
-  },
-  "type": {
-    args: [{pos: 0, name: "value", required: true}],
-    minArgs: 1,
-    maxArgs: 1
-  },
-  "ext": {
-    args: [{pos: 0, name: "exitCode", required: false}],
-    minArgs: 0,
-    maxArgs: 1
-  },
-  "lngth": {
-    args: [{pos: 0, name: "text", required: true}],
-    minArgs: 1,
-    maxArgs: Infinity
-  },
-  "steal": {
-    args: [
-      {pos: 0, name: "text", required: true},
-      {pos: 1, name: "start", required: true},
-      {pos: 2, name: "end", required: false}
-    ],
-    minArgs: 2,
-    maxArgs: 3
-  }
-    
+  "wait": {args: [{pos: 0, name: "unit", required: false, type: "txt"}, {pos: 1, name: "duration", required: true, type: ["int", "dint"]}], minArgs: 1, maxArgs: 2},
+  "wrt": {args: [{pos: 0, required: true}], minArgs: 1, maxArgs: Infinity},
+  "tlk": {args: [{pos: 0, name: "type", required: true}], minArgs: 1, maxArgs: Infinity},
+  "ret": {minArgs: 0, maxArgs: Infinity},
+  "type": {args: [{pos: 0, name: "value", required: true}], minArgs: 1, maxArgs: 1},
+  "ext": {args: [{pos: 0, name: "exitCode", required: false, type: "int"}], minArgs: 0, maxArgs: 1},
+  "lngth": {args: [{pos: 0, name: "text", required: true, type: "txt"}], minArgs: 1, maxArgs: Infinity},
+  "steal": {args: [{pos: 0, name: "text", required: true, type: "txt"}, {pos: 1, name: "start", required: true, type: "txt"}, {pos: 2, name: "end", required: false}], minArgs: 2, maxArgs: 3},
+  "trim": {args: [{pos: 0, name: "text", required: true, type: "txt"}, {pos: 1, name: "slice", required: true, type: "txt"}, {pos: 2, name: "type", required: true}], minArgs: 3, maxArgs: 3} 
 }
 async function evaluate(node) {
-  if(node.type === "Literal") { 
-    return node.value;
-  }
-  else if(node.type === "VariableReference") { 
-    if(node.name in database) {
+  switch(node.type) {
+    case "Literal":
+      return node.value;
+      break;
+    case "VariableReference":
+      if(node.name in database) {
+        return database[node.name];
+      }
+      else {
+        throw new Error("There is no found variable with the name '"+ node.name +"'. Please declare that variable before retrying.") // error code 9
+      }
+      break;
+    case "BinaryOperation":
+      let L = evaluate(node.left);
+      let R = evaluate(node.right);
+      let operator = node.operator;
+      switch(operator) { 
+        case '+': return L + R; break; case '-': return L - R; break;
+        case '*': return L * R; break; case '/': return L / R; break; case '%': return L % R; break;
+        case '^': return Math.pow(L, R); break;
+        case '+++': return L && R; break; case '!': return !L; break; case '|+|': return L || R; break; case '|-|': return (L && !R) || (!L && R); break;
+        case '?': return L === R; break; case '>': return L > R; break; case '<': return L < R; break;
+        case '>?': return L >= R; break; case '<?': return L <= R; break;
+        case '!>': return !(L > R); break; case '!<': return !(L < R); break; case '!?': return L == R; break;
+        case '!>?': return !(L >= R); break; case '!<?': return !(L <= R); break;
+      }
+      break;
+    case "ParameterReference":
       return database[node.name];
-    }
-    else {
-      throw new Error("There is no found variable with the name '"+ node.name +"'. Please declare that variable before retrying.") // error code 9
-    }
-  }
-  else if(node.type === "BinaryOperation") {
-    let L = evaluate(node.left);
-    let R = evaluate(node.right);
-    let operator = node.operator;
-    switch(operator) { 
-      case '+': return L + R;
-      case '-': return L - R;
-      case '*': return L * R;
-      case '/': return L / R;
-      case '%': return L % R;
-      case '^': return Math.pow(L, R);
-      case '+++': return L && R;
-      case '-+-': return !L;
-      case '|+|': return L || R;
-      case '|-|': return (L && !R) || (!L && R);
-      case '?': return L == R;
-      case '??': return L === R;
-    }
-  }
-  else if(node.type === "ParameterReference") { 
-    return database[node.name];
+      break;
   }
 }
 // functions ---
 function getType(value) {
-  if(typeof value === "number") {
-    return Number.isInteger(value) ? "int" : "dint";
-  }
-  else if(typeof value === "string") {
-    return "txt";
-  }
-  else if(typeof value === "boolean") {
-    return "stt";
-  }
-  else if(Array.isArray(value)) {
-    return "arr";
-  }
-  else if(typeof value === "object") {
-    return "obj";
-  }
-  else {
-    return "unknown";
+  switch(typeof value) {
+    case "number":
+      if(Number.isInteger(value)) {
+        return "int";
+      } else {return "dint"};
+      break;
+    case "string":
+      return "txt";
+      break;
+    case "boolean":
+      return "stt";
+      break;
+    case "object":
+      if(value !== null && Array.isArray(value)) {return "arr"};
+      else if(value !== null && !(Array.isArray(value))) {return "obj"};
+      else {return "nil"};
+      break;
+    default:
+      return "unknown";
+      break;
   }
 }
 function wrt(args) {
@@ -164,7 +129,7 @@ function steal(args) {
   const start = args[1];
   const end = args[2];
   if(getType(text) !== "txt" && getType(text) !== "int" && getType(text) !== "dint") {
-    throw new Error("It seems a steal function has a non-alphanumerical first argument. Please change it either or an integer, a dinteger or a string before retrying.");
+    throw new Error("It seems a steal function has a non-alphanumerical first argument. Please change it to either or an integer, a dinteger or a string before retrying.");
   }
   return text.slice(start, end);
 }
@@ -175,39 +140,43 @@ function occur(args) {
 }
 // AST statement executer ---
 async function execute(node) {
-  if(node.type === "VariableDeclaration") {
-    let name = node.variableName;
-    let value = evaluate(node.value);
-    database[name] = value;
-  }
-  if (node.type === "FunctionCall") {
-    if(node.name === "wrt") {
-      const evalArgs = node.arguments.map(args => await evaluate(args));
-      wrt(evalArgs);
-    }
-    else if(node.name === "tlk") {
-      const evalArgs = node.arguments.map(args => await evaluate(args));
-      return tlk(evalArgs);
-    }
-    else if(node.name === "ret") {
-      const evalArgs = node.arguments.map(args => await evaluate(args));
-      return ret(evalArgs);
-    }
-    else if(node.name === "ext") {
-      let exitCode = 0;
-      if(node.arguments.length > 0) {
-      exitCode = await evaluate(node.arguments[0]);
+  switch(node.type) {
+    case "VariableDeclaration":
+      let name = node.variableName;
+      let value = evaluate(node.value);
+      database[name] = value;
+      break;
+    case "FunctionCall":
+      switch(node.name) {
+        case "wrt":
+          const evalArgs = node.arguments.map(args => await evaluate(args));
+          wrt(evalArgs);
+          break;
+        case "tlk":
+          const evalArgs = node.arguments.map(args => await evaluate(args));
+          return tlk(evalArgs);
+          break;
+        case "ret":
+          const evalArgs = node.arguments.map(args => await evaluate(args));
+          return ret(evalArgs);
+          break;
+        case "ext":
+          let exitCode = 0;
+          if(node.arguments.length > 0) {
+          exitCode = await evaluate(node.arguments[0]);
+          throw new Error("Program exited with error code " + exitCode +".");
+          break;
+        case "wait";
+          const evalArgs = node.arguments.map(arg => await evaluate(arg));
+          await wait(evalArgs);
+          break;
+        case "lngth":
+          const evalArgs = node.arguments.map(arg => await evaluate(arg));
+          return lgnth(evalArgs);
+          break;
       }
-      throw new Error("Program exited with error code " + exitCode +".");
-    }
-    else if(node.name === "wait") {
-      const evalArgs = node.arguments.map(arg => await evaluate(arg));
-      await wait(evalArgs);
-    }
-    else if(node.name === "lngth") {
-      const evalArgs = node.arguments.map(arg => await evaluate(arg));
-      return lgnth(evalArgs);
+      break;
     }
   }
 }
-
+module.exports = {execute};
